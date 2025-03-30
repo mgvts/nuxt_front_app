2ebdb87990675e341f37285d1d5bf6d74fc95eb3
mrgen <mrgeniysak4795@gmail.com>
2025-03-30 19:20:50 +0300

    test baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/fonts"],
   app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/nuxt_front_app/' : '/',
     head: {
       title: "Фронтенд 2024-2025 на Vue",
       htmlAttrs: {
