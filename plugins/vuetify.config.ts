6241e613cfb41fa1f17322f71c622de94519d86d
mrgen <mrgeniysak4795@gmail.com>
2025-04-04 22:11:36 +0300

    add vuetify

---
+++ plugins/vuetify.config.ts
@@ -0,0 +1,31 @@
+// vuetify.config.ts
+import { defineVuetifyConfiguration } from 'vuetify-nuxt-module/custom-configuration'
+
+const myTheme = {
+    dark: false,
+    colors: {
+      'yellow': '#DBFF00',
+      'violet': '#8E6FF8',
+      'white': '#FFFFFF',
+      'black': '#000000',
+
+
+      error: '#B00020',
+      info: '#2196F3',
+      success: '#4CAF50',
+      warning: '#FB8C00',
+    },
+    variables: {
+      'border-radius': '50px',
+    }
+  }
+
+
+export default defineVuetifyConfiguration({
+  theme: {
+    defaultTheme: 'myTheme',
+    themes: {
+        myTheme
+    }
+  }
+})
