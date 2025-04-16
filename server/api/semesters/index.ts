155f2ae59dedb6ef365c3b68c41fb34c332e55b9
mrgen <mrgeniysak4795@gmail.com>
2025-04-16 22:35:44 +0300

    test 5

--- server/api/semesters/index.ts
+++ server/api/semesters/index.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 
 
 export default defineEventHandler(async (event) => {
+  console.log('trigger semester server handler')
   const response = await axios.get("http://84.201.151.242:8080/api/v1/semesters", {
   });
   return response.data;
