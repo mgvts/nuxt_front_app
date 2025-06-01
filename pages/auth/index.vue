f77790b1c7de65cc19db3b1f87f16290829864a4
mrgen <mrgeniysak4795@gmail.com>
2025-06-01 20:36:54 +0300

    fix auth register request fix styles with btns header add router.push to submit on auth

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { LoginPayload, RegisterPayload } from '~/types/auth'
 import messages from './locale.json'
 
 const { t } = useI18n({ messages })
@@ -14,8 +15,20 @@ interface Field {
     isPassword: boolean
     showed: boolean
 }
-type Action = "login" | "register"
-type FiledMap = Record<Action, Field[]> 
+interface LoginMap {
+    login: Field
+    password: Field
+}
+interface RegisterMap {
+    name: Field
+    login: Field
+    password: Field
+    repeatPassword: Field
+}
+interface FiledMap {
+    login: LoginMap
+    register: RegisterMap
+} 
 
 
 const isRegister = ref(true)
@@ -38,57 +51,57 @@ const getHeaderLabel = computed(() => {
 const getFooterLabel = computed(() => {
     return footerLabelMap[getAction.value]
 })
-const getApiPayload = () => {
+const getApiPayload = (): RegisterPayload | LoginPayload => {
     if (isRegister.value) {
         return {
-            login: fields.value.login[0].value,
-            password: fields.value.login[1].value
+            name: fields.value.register.name.value,
+            login: fields.value.register.login.value,
+            password: fields.value.register.password.value
         }
     }
     return {
-        login: fields.value.login[0].value,
-        password: fields.value.login[1].value,
-        
+        login: fields.value.login.login.value,
+        password: fields.value.login.login.value,
     }
 }
 
 const lenRule = (v: string) => v && 2 < v.length && v.length < 100 || t('неверная длина')
 // TODO: это плохо, тут по хорошему нужна мапа а не списки, но переделывать впадлу
 const fields = ref<FiledMap>({
-    register: [
-        {
+    register: {
+        name: {
             value: '',
             label: t("ФИО"),
             rules: [
                 lenRule,
-                (v: string) => v && 6 < v.length && v.length < 100 && 2 <= v.split(/\s/).length && v.split(/\s/).length <= 5 || t('неверное фио') ],
+                (v: string) => v && 6 < v.length && v.length < 100 && 2 <= v.split(/\s/).length && v.split(/\s/).length <= 5 || t('неверное фио')],
             isPassword: false,
             showed: true
         },
-        {
+        login: {
             value: '',
             label: t("Логин"),
             rules: [lenRule],
             isPassword: false,
             showed: true
         },
-        {
+        password: {
             value: '',
             label: t("Пароль"),
             rules: [lenRule],
             isPassword: true,
             showed: false
         },
-        {
+        repeatPassword: {
             value: '',
             label: t("Подтвердите пароль"),
             rules: [lenRule],
             isPassword: true,
-            showed: false
-        }
-    ],
-    login: [
-        {
+
+        },
+    },
+    login: {
+        login: {
             value: '',
             label: t('Логин'),
             rules: [],
@@ -96,39 +109,38 @@ const fields = ref<FiledMap>({
             isPassword: false,
             showed: true
         },
-        {
+        password: {
             value: '',
             label: t("Пароль"),
             rules: [],
             // rules: [lenRule],
             isPassword: true,
             showed: false
-        },
-    ]
+        }
+    }
 })
 
 const submit = async () => {
-    console.log('call')
-    if (isRegister.value) {
-        await register({
-
+    const router = useRouter()
+    const payload = getApiPayload()
+    try {
+        if (isRegister.value) {
+            await register(payload as RegisterPayload)
+        } else {
+            await login(payload)
         }
-        )
-    }else {
-        await login({
-            login: fields.value.login[0].value,
-            password: fields.value.login[1].value
-
-        })
+        router.push('/')
+    } catch (e) {
+        console.error(e)
     }
 }
 </script>
 
 <template>
-    <button @click="isRegister = !isRegister">
+    <UIButton @click="isRegister = !isRegister">
         change form
-    </button>
-    <div class="py-10"/>
+    </UIButton>
+    <div class="py-10" />
 
     <UIForm>
         <template #header>
@@ -136,20 +148,13 @@ const submit = async () => {
         </template>
         <template #body>
             <div :class="['d-flex flex-column', isRegister ? 'ga-9' : 'ga-13']">
-                <UIInput
-                    v-for="field of fields[getAction]" 
-                    :key="field.label" 
-                    v-model="field.value" 
-                    :label="field.label"
-                    :rules="field.rules"
-                    :type="getFieldType(field)"
-                    :append-icon="getFieldShowedToggle(field)"
-                    @click:append="field.showed = !field.showed"
-                />
+                <UIInput v-for="field of fields[getAction]" :key="field.label" v-model="field.value"
+                    :label="field.label" :rules="field.rules" :type="getFieldType(field)"
+                    :append-icon="getFieldShowedToggle(field)" @click:append="field.showed = !field.showed" />
             </div>
         </template>
         <template #footer>
-            <UIButton :text="getFooterLabel" @click="submit"/>
+            <UIButton :text="getFooterLabel" @click="submit" />
         </template>
     </UIForm>
 
